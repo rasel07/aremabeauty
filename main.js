71874837469937567e58a161d27ade3e3702767e
Rasel Kabir <32257679+rasel07@users.noreply.github.com>
2026-03-11 05:47:25 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+<!-- WM Toolkit -->
+<script src="https://cdn.jsdelivr.net/gh/willmyerscode/toolkit@1/index.min.js"></script>
+<!-- Mega Menu Styles from Will-Myers -->
+<link href="https://cdn.jsdelivr.net/gh/willmyerscode/megaMenu@2/megaMenu.min.css" rel="stylesheet" />
+
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rasel07/aremabeauty@refs/heads/main/style.css">
